13c616dee85441c8661318bbb20d9c1c33db8b9a
Apurba Saha <47732829+diponsaha007@users.noreply.github.com>
2019-11-02 08:12:36 +0600

    5 done

--- Problem 5/Problem 5.py
+++ Problem 5/Problem 5.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Nov  2 04:06:02 2019
+Created on Sat Nov  2 03:49:14 2019
 
 @author: USER
 """
-
 
 #import sys, os
 #sys.path.append(os.path.abspath(os.path.join('..', 'Graph')))
@@ -20,6 +19,10 @@ import simplekml
 import math
 import time
 
+
+CAR_FARE = 20
+METRO_FARE = 5
+BUS_FARE = 7
 
 with open('../Dictionaries/node_dict.p', 'rb') as fp:
     nodes = pickle.load(fp)
@@ -44,13 +47,11 @@ with open('../Dictionaries/distance_dict.p', 'rb') as fp:
                                          
     
 
-
- #TODO : CHANGE WEIGHTS ##############################################
 with open('../edges_no_index.txt') as f:
     u = []
     v = []
     weights = []
-    
+    typ ={}
     while True:
         line = f.readline()
         if line == '':
@@ -58,32 +59,123 @@ with open('../edges_no_index.txt') as f:
         x = line.split()
         u.append(int(x[0]))
         v.append(int(x[1]))
-        weights.append(distance_dict[(u[-1],v[-1])])
+        weights.append( distance_dict[(u[-1],v[-1])] /10)
+        typ[(int(x[0]),int(x[1]))] = 1
+        typ[(int(x[1]),int(x[0]))] = 1
 
 for i in bus_u_dict:
     u.append(i[0])
     v.append(i[1])
-    weights.append(bus_u_dict[i][2])
+    weights.append( bus_u_dict[i][2]/10)
+    typ[(i[0],i[1])] = 2
+    typ[(i[1],i[0])] = 2
 
 for i in bus_b_dict:
     u.append(i[0])
     v.append(i[1])
-    weights.append(bus_b_dict[i][2])
+    weights.append( bus_b_dict[i][2]/10)
+    typ[(i[0],i[1])] = 2
+    typ[(i[1],i[0])] = 2
 
 for i in metro_dict:
     u.append(i[0])
     v.append(i[1])
-    weights.append(metro_dict[i][2])
+    weights.append( metro_dict[i][2]/10)
+    typ[(i[0],i[1])] = 3
+    typ[(i[1],i[0])] = 3
 
-
- #TODO : CHANGE WEIGHTS ##############################################
 
 
 edges = [g.Edge(u[i], v[i], weights[i], True) for i in range(len(u))]
-graph= g.Graph(nodes.values(), edges, lat_long,[])
+graph= g.Graph(nodes.values(), edges, lat_long,[],typ)
 
 
 ################## WRITE CODE HERE #################################
-
-
+def get_mod_time(t ):
+    if t[1]<=15:
+        return 15- t[1]
+    if t[1]<=30:
+        return 30- t[1]
+    if t[1]<=45:
+        return 45- t[1]
+    if t[1]<=60:
+        return 60- t[1]
+def add_minute(tt, m):
+    x= tt[0]
+    y= tt[1]
+    z = tt[2]
+    z+= m- math.floor(m)
+    m =  m- math.floor(m)
+    if(z>=60):
+        z-=60
+        y+=1
+    x+= math.floor(m/60)
+    m= m%60
+    y += m
+    if(y>=60):
+        x+= math.floor(y/60)
+        y =y%60
+    if(x>=24):
+        x %=24
+    return (x,y,z)
+def print_path_info2(cst,path,ini_time):
+        sz = len(path)
+        text =[]
+        text.append("Source:  ("+str(lat_long[path[0]][0])+ ", "+str(lat_long[path[0]][1])+")")
+        text.append("Destination:  ("+str(lat_long[path[sz-1]][0])+ ", "+str(lat_long[path[sz-1]][1])+")")
+        now_time = ini_time
+        for i in range(sz-1):
+            if i==0:
+                if (path[i],path[i+1]) in metro_dict:
+                    now_time = add_minute(now_time ,get_mod_time(now_time ))
+                    text.append("Time : "+str(now_time[0])+" : " +str(now_time[1])+". Cost: "+str(graph.adj_weights[(path[i],path[i+1])])+"time .  Ride Metro from Source "+str( metro_dict[(path[i],path[i+1])][1][0])+"("+str(lat_long[path[i]][0])+", "+str(lat_long[path[i]][1])+") to "+str( metro_dict[(path[i],path[i+1])][1][1])+ " ("+str(lat_long[path[i+1]][0])+", "+str(lat_long[path[i+1]][1])+")" )
+                elif (path[i],path[i+1]) in bus_u_dict:
+                    now_time = add_minute(now_time ,get_mod_time(now_time ))
+                    text.append("Time : "+str(now_time[0])+" : " +str(now_time[1])+". Cost: "+str(graph.adj_weights[(path[i],path[i+1])])+"time .  Ride Uttara Bus from Source "+str( bus_u_dict[(path[i],path[i+1])][1][0])+"("+str(lat_long[path[i]][0])+", "+str(lat_long[path[i]][1])+") to "+str( bus_u_dict[(path[i],path[i+1])][1][1])+ " ("+str(lat_long[path[i+1]][0])+", "+str(lat_long[path[i+1]][1])+")" )
+                elif (path[i],path[i+1]) in bus_b_dict:
+                    now_time = add_minute(now_time ,get_mod_time(now_time ))
+                    text.append("Time : "+str(now_time[0])+" : " +str(now_time[1])+". Cost: "+str(graph.adj_weights[(path[i],path[i+1])])+"time .  Ride Bikolpo Bus from Source "+str( bus_b_dict[(path[i],path[i+1])][1][0])+"("+str(lat_long[path[i]][0])+", "+str(lat_long[path[i]][1])+") to "+str( bus_b_dict[(path[i],path[i+1])][1][1])+ " ("+str(lat_long[path[i+1]][0])+", "+str(lat_long[path[i+1]][1])+")" )
+                
+                else:
+                    text.append("Time : "+str(now_time[0])+" : " +str(now_time[1])+". Cost: "+str(graph.adj_weights[(path[i],path[i+1])])+"time . Ride Car from Source ("+str(lat_long[path[i]][0])+", "+str(lat_long[path[i]][1])+") to ("+str(lat_long[path[i+1]][0])+", "+str(lat_long[path[i+1]][1])+")" )
+            elif i== sz-2:
+                if (path[i],path[i+1]) in metro_dict:
+                    now_time = add_minute(now_time ,get_mod_time(now_time ))
+                    text.append("Time : "+str(now_time[0])+" : " +str(now_time[1])+". Cost: "+str(graph.adj_weights[(path[i],path[i+1])])+"time . Ride Metro from "+str( metro_dict[(path[i],path[i+1])][1][0])+"("+str(lat_long[path[i]][0])+", "+str(lat_long[path[i]][1])+") to Destination "+str( metro_dict[(path[i],path[i+1])][1][1])+ " ("+str(lat_long[path[i+1]][0])+", "+str(lat_long[path[i+1]][1])+")" )
+                elif (path[i],path[i+1]) in bus_u_dict:
+                    now_time = add_minute(now_time ,get_mod_time(now_time ))
+                    text.append("Time : "+str(now_time[0])+" : " +str(now_time[1])+". Cost: "+str(graph.adj_weights[(path[i],path[i+1])])+"time .  Ride Uttara Bus from  "+str( bus_u_dict[(path[i],path[i+1])][1][0])+"("+str(lat_long[path[i]][0])+", "+str(lat_long[path[i]][1])+") to Destination "+str( bus_u_dict[(path[i],path[i+1])][1][1])+ " ("+str(lat_long[path[i+1]][0])+", "+str(lat_long[path[i+1]][1])+")" )
+                elif (path[i],path[i+1]) in bus_b_dict:
+                    now_time = add_minute(now_time ,get_mod_time(now_time ))
+                    text.append("Time : "+str(now_time[0])+" : " +str(now_time[1])+". Cost: "+str(graph.adj_weights[(path[i],path[i+1])])+"time .  Ride Bikolpo Bus from  "+str( bus_b_dict[(path[i],path[i+1])][1][0])+"("+str(lat_long[path[i]][0])+", "+str(lat_long[path[i]][1])+") to Destination "+str( bus_b_dict[(path[i],path[i+1])][1][1])+ " ("+str(lat_long[path[i+1]][0])+", "+str(lat_long[path[i+1]][1])+")" )
+                
+                else:
+                    text.append("Time : "+str(now_time[0])+" : " +str(now_time[1])+". Cost: "+str(graph.adj_weights[(path[i],path[i+1])])+"time . Ride Car from ("+str(lat_long[path[i]][0])+", "+str(lat_long[path[i]][1])+") to Destination ("+str(lat_long[path[i+1]][0])+", "+str(lat_long[path[i+1]][1])+")" )
+                
+                
+            else:
+                if (path[i],path[i+1]) in metro_dict:
+                    now_time = add_minute(now_time ,get_mod_time(now_time ))
+                    text.append("Time : "+str(now_time[0])+" : " +str(now_time[1])+". Cost: "+str(graph.adj_weights[(path[i],path[i+1])])+"time . Ride Metro from "+str( metro_dict[(path[i],path[i+1])][1][0])+"("+str(lat_long[path[i]][0])+", "+str(lat_long[path[i]][1])+") to "+str( metro_dict[(path[i],path[i+1])][1][1])+ " ("+str(lat_long[path[i+1]][0])+", "+str(lat_long[path[i+1]][1])+")" )
+                elif (path[i],path[i+1]) in bus_u_dict:
+                    now_time = add_minute(now_time ,get_mod_time(now_time ))
+                    text.append("Time : "+str(now_time[0])+" : " +str(now_time[1])+". Cost: "+str(graph.adj_weights[(path[i],path[i+1])])+"time .  Ride Uttara Bus from  "+str( bus_u_dict[(path[i],path[i+1])][1][0])+"("+str(lat_long[path[i]][0])+", "+str(lat_long[path[i]][1])+") to "+str( bus_u_dict[(path[i],path[i+1])][1][1])+ " ("+str(lat_long[path[i+1]][0])+", "+str(lat_long[path[i+1]][1])+")" )
+                elif (path[i],path[i+1]) in bus_b_dict:
+                    now_time = add_minute(now_time ,get_mod_time(now_time ))
+                    text.append("Time : "+str(now_time[0])+" : " +str(now_time[1])+". Cost: "+str(graph.adj_weights[(path[i],path[i+1])])+"time .  Ride Bikolpo Bus from  "+str( bus_b_dict[(path[i],path[i+1])][1][0])+"("+str(lat_long[path[i]][0])+", "+str(lat_long[path[i]][1])+") to "+str( bus_b_dict[(path[i],path[i+1])][1][1])+ " ("+str(lat_long[path[i+1]][0])+", "+str(lat_long[path[i+1]][1])+")" )
+                
+                else:
+                    text.append("Time : "+str(now_time[0])+" : " +str(now_time[1])+". Cost: "+str(graph.adj_weights[(path[i],path[i+1])])+"time . Ride Car from  ("+str(lat_long[path[i]][0])+", "+str(lat_long[path[i]][1])+") to ("+str(lat_long[path[i+1]][0])+", "+str(lat_long[path[i+1]][1])+")" )
+            now_time = add_minute(now_time, graph.adj_weights[(path[i],path[i+1])]*60)
+        return text
+    
+    
+    
+orig_node = 1423
+dest_node = 2
+ini_time = (13,00)
+dist, parent = graph.dijkstra(orig_node, ini_time )
+path = graph.get_path(parent,dest_node)
+print(path)
+print_text = print_path_info2(dist[dest_node],path,(ini_time[0],ini_time[1],0))
 
